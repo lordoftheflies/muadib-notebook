492c5639ef9175a21654760f9f43cef5347fd9d3
László Hegedűs <heglas11@gmail.com>
2018-03-27 04:35:49 +0200

    fix #1: notebook as a module or execute

---
+++ tests/test_hook.py
@@ -0,0 +1,46 @@
+import unittest
+
+import os
+from importlib._bootstrap_external import _NamespacePath
+
+import muadib_notebook
+
+
+class TestHook(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_module_methods_and_functions(self):
+        import notebooks.test_notebook as given_module
+        self.assertListEqual(dir(given_module), [
+            'SampleClass',
+            '__builtins__',
+            '__doc__',
+            '__file__',
+            '__loader__',
+            '__name__',
+            '__package__',
+            '__spec__',
+            'get_ipython',
+            'sample_dictionary',
+            'sample_float',
+            'sample_instance',
+            'sample_integer',
+            'sample_list',
+            'sample_object',
+            'sample_strng',
+            'sample_tuple',
+        ])
+
+    def test_execute(self):
+        print(os.path.abspath(os.path.curdir))
+        with muadib_notebook.NotebookExecutor(
+                path=os.path.join('..','notebooks'),
+                fullname='test_notebook'
+        ) as e:
+            e()
+
+
+if __name__ == '__main__':
+    unittest.main()
